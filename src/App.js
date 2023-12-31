0c1622e315f5550e1e0630d5b762ac41558f67e1
tejas113 <tejaskalla12@gmail.com>
2023-12-31 16:22:49 +0000

    RockPaperScissors

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <div>Hello World</div>
+
+export default App
